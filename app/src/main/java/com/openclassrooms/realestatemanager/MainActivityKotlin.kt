befac9598d17d529d33393146510088a1dfe580d
zainm <zainmahmoo@live.co.uk>
2022-02-04 08:55:24 +0000

    Refactor so i can use Kotlin

---
+++ app/src/main/java/com/openclassrooms/realestatemanager/MainActivityKotlin.kt
@@ -0,0 +1,11 @@
+package com.openclassrooms.realestatemanager
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivityKotlin : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main_kotlin)
+    }
+}
